004ee79877c216dcb883780e9951916f1d62356d
Mathieu Veber <43341681+MathieuVeber@users.noreply.github.com>
2021-05-07 09:15:42 +0200

    Rename kuzzle-reloader to ergol, extract reloader functions, add github action & workflows (#1)
    # What does this PR do?
    
    - Rename kuzzle-reloader to ergol
    - Extract reloader functions
    - Add proper cli options with flags
    - Add config file handling
    - Adds a linter and ensure that there is no lint errors for each PR.
    - Also automates the release on npm when pushing on master.
    
    ## Workflows steps
    
    - Pull request: lint test
    - Push on master: lint test and (if successful) deployment on NPM

--- README.md
+++ README.md
@@ -1,27 +1,82 @@
-# Kuzzle Reloader
+# Ergol
 
 __For development purposes only__
 
-This script is meant to be run on top of a Kuzzle starter script. It watches source files (json, ts, js) from Kuzzle core, plugins, configuration files. When a file changes, it automatically shuts the current process down, __WAIT__ until it has correctly been stopped (or kills it after some delay), and only then restarts it.
+This script is meant to run on top of another process while watching source files. If any change occurs, it automatically shuts the process down, __WAIT__ until it has correctly been stopped (or kills it after some delay), and only then restarts it.
 
 This script is meant to replace solutions like nodemon, which have a few cumbersome caveats.
 
 
 ## Usage
 
-` $ npx kuzzle-reloader <config file> <node args> <starter script> <args>
-`
+`$ npx ergol <script> -c <config> -d <cwd>`
 
-| Arguments      | Description                               |
-|----------------|-------------------------------------------|
-| config files   | This script configuration file            |
-| node args      | Arguments to pass to the node interpreter |
-| starter script | The script to execute using node          |
-| args           | Script arguments                          |
+| Options         | Alias | Required | Description                                                             |
+|-----------------|-------|----------|-------------------------------------------------------------------------|
+| No flag needed  | -     | yes      | Script that runs under this process and that is reloaded when necessary |
+| `--config`      | `-c`  | no       | JSON file containing configuration variables intended to override default ones |
+| `--cwd`         | `-d`  | no       | Current Working Directory (if different from your actual cwd)           |
+| `--script-args` | -     | no       | Arguments to pass to the script                                         |
+| `--node-args`   | -     | no       | Arguments to pass to the node interpreter                               |
+| `--watch`       | `-w`  | no       | Array of cwd relative path/to/directories or files to watch             |
+| `--kill-delay`  | `-k`  | no       | Kill the process if it did not stop after this delay                    |
+
+
+## Configuration
+
+Ergol allows you to customize its behavior when necessary. If its a ponctual setting you can use one of the command argument listed before but we recommend using a config file when using recurrent options.
+
+If you choose to use both methods, beware of which setting will be selected. Ergol will resolve each parameter by looking for command options first, config file then and default config otherwise.
+
+### Default Config
+
+```json
+{
+  "killDelay": "5000",
+  "nodeArgs": [],
+  "scriptArgs": [],
+  "watch": [
+    "lib",
+    "src",
+    "index.*s",
+    "nodes_modules"
+  ]
+}
+```
 
 
 ## Example
 
 ```sh-session
-$ npx kuzzle-reloader --inspect=0.0.0.0:9229 -r ts-node/register docker/scripts/start-kuzzle-dev.ts --enable-plugins functional-test-plugin
+$ npx ergol scripts/start-app.js -c config/ergol.config.json --script-args=--option my-option-1
+```
+
+### ergol.config.json
+
+```json
+{
+  "killDelay": "10000",
+  "watch": [
+    "index.ts",
+    "src",
+    "package-lock.json"
+  ]
+}
+```
+
+### Final Ergol Configuration
+
+```json
+{
+  "killDelay": "10000",
+  "nodeArgs": [],
+  "scriptArgs": [
+    "--option my-option-1"
+  ],
+  "watch": [
+    "index.ts",
+    "src",
+    "package-lock.json"
+  ]
+}
 ```
---
+++ src/index.js
@@ -0,0 +1,151 @@
+/*
+ * Kuzzle, a backend software, self-hostable and ready to use
+ * to power modern apps
+ *
+ * Copyright 2015-2021 Kuzzle
+ * mailto: support AT kuzzle.io
+ * website: http://kuzzle.io
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/* eslint-disable no-console */
+
+'use strict';
+
+const path = require('path');
+const clc = require('cli-color');
+const chokidar = require('chokidar');
+const { fork } = require('child_process');
+const StateEnum = require('./stateEnum');
+
+/**
+ * Process running under this one and that is reloaded when necessary
+ */
+let childProcess;
+
+/**
+ * ChildProcess current state
+ */
+let state = StateEnum.STOPPED;
+
+/**
+ * Execute a script and run on top it while watching requested files.
+ * When a file changes, it automatically shuts the current process down,
+ * wait until it has correctly been stopped (or kills it after some delay),
+ * and only then restarts it.
+ * 
+ * @param {Object} config Configuration variables
+ * @param {string} script Path to the script that runs under this process and that is reloaded when necessary
+ * @param {Array<string>} scriptArgs Arguments to pass to the script (Default to [])
+ * @param {Array<string>} nodeArgs Arguments to pass to the node interpreter (Default to [])
+ */
+async function run(config, script) {
+  const watcher = chokidar.watch(script);
+  watcher.add(config.watch.map(dir => path.join(config.cwd, dir)));
+
+  watcher.on('change', async file => {
+    if (state !== StateEnum.STOPPING) {
+      console.log(clc.green(`[RELOADER] Change detected on ${path.relative(config.cwd, file)}. Reloading...`));
+      await stopProcess(config.killDelay);
+      startProcess(script, config.scriptArgs, config.nodeArgs);
+    }
+  });
+
+  process.on('SIGUSR1', async () => {
+    console.log(clc.green('[RELOADER] Caught signal SIGUSR1. Restarting...'));
+    await stopProcess(config.killDelay);
+    startProcess(script, config.scriptArgs, config.nodeArgs);
+  });
+
+  startProcess(script, config.scriptArgs, config.nodeArgs);
+}
+
+/**
+ * Starts the child process with the given args
+ * 
+ * @param {string} script Path to the script that runs under this process and that is reloaded when necessary
+ * @param {Array<string>} scriptArgs Arguments to pass to the script (Default to [])
+ * @param {Array<string>} nodeArgs Arguments to pass to the node interpreter (Default to [])
+ */
+function startProcess (script, scriptArgs, nodeArgs) {
+  childProcess = fork(script, scriptArgs, {
+    detached: true,
+    execArgv: nodeArgs,
+  });
+
+  childProcess.on('exit', (code, signal) => {
+    const msg = code !== null
+      ? `code ${code}`
+      : `signal ${signal}`;
+
+    console.log(clc.red(`[RELOADER] Process exited with ${msg}. Waiting for a file change to restart it.`));
+    childProcess = null;
+    state = StateEnum.STOPPED;
+  });
+
+  // Exits child process on termination
+  for (const signal of ['SIGINT', 'SIGTERM']) {
+    // eslint-disable-next-line no-loop-func
+    process.on(signal, () => {
+      if (childProcess) {
+        childProcess.kill(signal);
+      }
+
+      process.exit();
+    });
+  }
+
+  state = StateEnum.RUNNING;
+}
+
+/**
+ * Gracefully shuts the current process down or kills it after some delay
+ * 
+ * @param {number} killDelay Max delay after which the process must be killed
+ */
+async function stopProcess (killDelay) {
+  if (state !== StateEnum.RUNNING) {
+    return;
+  }
+
+  state = StateEnum.STOPPING;
+
+  childProcess.removeAllListeners();
+
+  let exited = false;
+
+  childProcess.on('exit', () => {
+    exited = true;
+  });
+
+  childProcess.kill();
+
+  // Wait a certain delay for the process to stop itself and if time is up, kills it
+  const now = Date.now();
+  let forced = false;
+
+  while (!exited) {
+    await new Promise(resolve => setTimeout(resolve, 200));
+
+    if (!forced && (Date.now() - now) > killDelay) {
+      console.log(clc.red(`[RELOADER] Process still here after ${killDelay}ms. Sending a SIGKILL signal`));
+      childProcess.kill('SIGKILL');
+      forced = true;
+    }
+  }
+
+  state = StateEnum.STOPPED;
+}
+
+module.exports = run;
